7b0bef4240e63eec132a3aa301a9e33ac031ef08
LeveyLeonhardt <edwarddu1996@gmail.com>
2018-12-18 20:25:40 -0500

    Init version of physical register file

--- project3/verilog/PhysRegFile.v
+++ project3/verilog/PhysRegFile.v
@@ -1,11 +1,38 @@
 `include "config.v"
-
+`define LOG_PHYS    $clog2(NUM_PHYS_REGS)
 
 module PhysRegFile (
-    /* Write Me */
+    input CLK,
+    input RESET,
+    input [`LOG_PHYS - 1 : 0] RegAddrA_IN,
+    input [`LOG_PHYS - 1 : 0] RegAddrB_IN,
+    input [`LOG_PHYS - 1 : 0] RegAddrC_IN,
+    input [`LOG_PHYS - 1 : 0] RegWrite_IN,
+    input [31:0] DataWrite_IN,
+    input [0:0] Write_IN, // 0 = don't write, 1 = write
+    output [31:0] RegValueA_OUT,
+    output [31:0] RegValueB_OUT,
+    output [31:0] RegValueC_OUT,
     );
 	 
 	reg [31:0] PReg [NUM_PHYS_REGS-1:0] /*verilator public*/;
+    wire temp;
+
+    assign RegValueA_OUT = PReg[RegAddrA_IN];
+    assign RegValueB_OUT = PReg[RegAddrB_IN];
+    assign RegValueC_OUT = PReg[RegAddrC_IN];
+    always @(posedge CLK or negedge RESET) begin
+	    if (!RESET) begin
+            temp = 0;
+            while (temp < NUM_PHYS_REGS) begin
+                PReg[temp] = 0;
+                temp = temp + 1;
+            end
+        end
+        if (Write_IN) begin
+            assign PReg[RegWrite_IN] = DataWrite_IN;
+        end
+    end
 
     /* Write Me */
     
--- project3/verilog/RegRead.v
+++ project3/verilog/RegRead.v
@@ -6,8 +6,17 @@ module RegRead#(
     parameter NUM_PHYS_REGS = 64 
 )
 (
-
-    /* Write Me */
+    input CLK,
+    input RESET,
+    input [`LOG_PHYS - 1 : 0] RegAddrA_IN,
+    input [`LOG_PHYS - 1 : 0] RegAddrB_IN,
+    input [`LOG_PHYS - 1 : 0] RegAddrC_IN,
+    input [`LOG_PHYS - 1 : 0] RegWrite_IN,
+    input [31:0] DataWrite_IN,
+    input [0:0] Write_IN, // 0 = don't write, 1 = write
+    output [31:0] RegValueA_OUT,
+    output [31:0] RegValueB_OUT,
+    output [31:0] RegValueC_OUT,
 
     );
 
@@ -15,7 +24,17 @@ module RegRead#(
 	.NUM_PHYS_REGS(NUM_PHYS_REGS)
 	)
 	PhysRegFile(
-    /* Write Me */
+        .CLK(CLK),
+        .RESET(RESET),
+        .RegAddrA_IN(RegAddrA_IN),
+        .RegAddrB_IN(RegAddrB_IN),
+        .RegAddrC_IN(RegAddrC_IN),
+        .RegWrite_IN(RegWrite_IN),
+        .DataWrite_IN(DataWrite_IN),
+        .Write_IN(Write_IN),
+        .RegValueA_OUT(RegValueA_OUT),
+        .RegValueB_OUT(RegValueB_OUT),
+        .RegValueC_OUT(RegValueC_OUT)
     );
     
     /* Write Me */
